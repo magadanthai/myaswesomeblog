0f2491ba3c19e1a61559ad70bd94ea162658efa8
doctor.madbee@gmail.com <Password@12>
2021-05-30 23:43:08 +0700

    mssql is working!

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateTimeField()
+    text = models.CharField(max_length=300)
+    image = models.ImageField(upload_to='blog_img')
