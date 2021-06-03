298ca220f9d87c60e6c30c57e072e09db8eb9bf0
magadanthai <doctor.madbee@gmail.com>
2021-06-03 11:57:13 +0700

    Relation between Post and Posts Change description for admin blogs

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,6 @@ class Post(models.Model):
     date = models.DateTimeField()
     text = models.CharField(max_length=300)
     image = models.ImageField(upload_to='blog_img')
+
+    def __str__(self):
+        return self.title
